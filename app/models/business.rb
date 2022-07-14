b6d0829917b1b58fae66d9642216b6da47e80792
acorzo <alexecorzo@gmail.com>
2022-07-14 15:25:38 -0500

    Fix landing, delete index.scss, and refactor Devise

--- app/models/business.rb
+++ app/models/business.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class Business < ApplicationRecord
+  # Create a business for user admin
+  belongs_to :user
   validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
   root 'home#index'
+  devise_scope :user do
+    get "users", to: "devise/sessions#new"
+  end
+  devise_for :users
+  
   resources :stylists
   resources :businesses
   resources :catalogs
