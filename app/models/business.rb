bc8ee93433f8ac45530c41fd797799d29040405b
acorzo <alexecorzo@gmail.com>
2022-07-27 12:37:32 -0500

    Improve style and logic of customers

--- app/models/business.rb
+++ app/models/business.rb
@@ -2,7 +2,7 @@
 
 # Create a Business for user
 class Business < ApplicationRecord
-  belongs_to :user
+  belongs_to :user, inverse_of: :business
   has_many :stylists, dependent: :destroy
 
   validates :name, presence: true
