776ef386da96c45982cd81a930fa2f97a2450ba0
acorzo <alexecorzo@gmail.com>
2022-08-04 17:43:29 -0500

    Redirect user after sign_out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index]
+
+
+  private
+
+  # Overwriting the sign_out redirect path method
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
