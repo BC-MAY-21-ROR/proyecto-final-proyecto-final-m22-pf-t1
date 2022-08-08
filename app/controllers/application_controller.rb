ed2a6d3255b8872b99ec60bb2fc8028fcb2c5ef1
d1eg0dev <diego@studiowd.com.mx>
2022-08-08 16:18:31 -0500

    Button for login in the sidebar nav when the user sign out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index]
 
   private
-  def after_sign_in_path_for(resource_or_scope)     
-    dashboard_index_path   
+  def after_sign_in_path_for(resource_or_scope)
+    dashboard_index_path
+  end
+
+  private
+  def after_sign_out_path_for(resource_or_scope)
+    root_path
   end
 end
