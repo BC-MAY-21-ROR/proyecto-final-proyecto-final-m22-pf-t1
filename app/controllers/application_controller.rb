aa9421ee51647f905f5f678f36cc08bab7d8af55
acorzo <alexecorzo@gmail.com>
2022-08-08 17:26:14 -0500

    Applying ruby style guide with rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index]
 
   private
-  def after_sign_in_path_for(resource_or_scope)
+
+  def after_sign_in_path_for(_resource_or_scope)
     dashboard_index_path
   end
 
-  def after_sign_out_path_for(resource_or_scope)
+  def after_sign_out_path_for(_resource_or_scope)
     root_path
   end
 end
