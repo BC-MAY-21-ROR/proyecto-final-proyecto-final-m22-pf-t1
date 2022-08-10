ef141170b64a49ada3b9882570232364853d15ed
Juan Martín Quintero Ballí <103448789+Jumaquba@users.noreply.github.com>
2022-08-10 15:04:52 -0500

    method calculate comisions

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,6 +4,8 @@ class ReportsController < ApplicationController
   def index
     @sales = invoice_daily_report
     @sales_monthly = invoices_monthly_report
+    calculate_earn(80)
+  
   end
 
   def invoice_daily_report
@@ -22,5 +24,29 @@ class ReportsController < ApplicationController
       @sub = i.amount + @sub
     end
     @sub
+  end
+
+  def stylists_on_invoice(inv_id)
+   @stylists_on= OrderItem.select('DISTINCT stylist_id').where('invoice_id = ?',inv_id)
+  end
+
+  def items_in_invoice(inv_id,sty_id)
+    @items_by_stylist=OrderItem.where(stylist_id: sty_id).where(invoice_id: inv_id)
+  end
+
+
+  def calculate_earn(inv_id)
+    @datas=Hash.new
+    stylists_on_invoice(inv_id)
+    @stylists_on.each do |stylist|
+    subt=0
+    gain=0
+      items_in_invoice(inv_id, stylist.stylist_id)
+        @items_by_stylist.each do |item|
+          gain += ((item.product.price * item.quantity)+item.adjustment)*(1 - item.product.cannon)
+           subt += item.product.price
+        end
+       @datas[stylist.stylist.name]= [subt, gain]
+    end
   end
 end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -5,4 +5,5 @@ class OrderItem < ApplicationRecord
   belongs_to :invoice
   belongs_to :product
   belongs_to :stylist
+
 end
