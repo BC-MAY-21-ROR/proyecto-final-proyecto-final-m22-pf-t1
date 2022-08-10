069db92b60b27c5a33b09f29dc97bdee9177d535
acorzo <alexecorzo@gmail.com>
2022-08-10 15:38:52 -0500

    Fix associations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,6 +3,7 @@
 # Create a customer for business
 class Customer < ApplicationRecord
   belongs_to :business
+  has_many :invoices
 
   scope :with_name_or_dni, lambda { |customer_search|
                              where('lower(full_name) LIKE ? OR dni LIKE ?', "%#{customer_search.downcase}%",
