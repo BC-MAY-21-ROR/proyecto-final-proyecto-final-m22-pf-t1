553420ee1ed74a0b2c53c94668f2763cdfb70a27
acorzo <alexecorzo@gmail.com>
2022-08-12 03:39:39 -0500

    Fix delete order_items action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,20 @@
 
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:index]
+
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
   end
 
-  private
-
-  def after_sign_in_path_for(_resource_or_scope)
+  protected
+  
+  def after_sign_in_path_for(resource)
     dashboard_index_path
   end
 
-  def after_sign_out_path_for(_resource_or_scope)
+  def after_sign_out_path_for(resource_or_scope)
     root_path
   end
+
+
 end
