ed825b1d891b3346cc28d36fe85c16f578b86bb8
Juan Martín Quintero Ballí <103448789+Jumaquba@users.noreply.github.com>
2022-08-16 16:28:36 -0500

    module payments  working

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,7 +4,7 @@
 class Payment < ApplicationRecord
   belongs_to :invoice
 
-  validates :date, :value, :type, presence: true
-  validates :amount, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :date, :value, :kind, presence: true
+  validates :value, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :note, length: { maximum: 200 }
 end
