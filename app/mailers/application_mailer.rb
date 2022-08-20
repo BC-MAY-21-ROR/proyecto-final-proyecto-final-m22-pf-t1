8f63b33ceae227bdb62e98a31b8847a3e4ab33a7
d1eg0dev <diego@studiowd.com.mx>
2022-08-19 19:58:15 -0500

    end mailers

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 module Mailers
-class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
-  layout 'mailer'
+  class ApplicationMailer < ActionMailer::Base
+    default from: 'from@example.com'
+    layout 'mailer'
+  end
 end
