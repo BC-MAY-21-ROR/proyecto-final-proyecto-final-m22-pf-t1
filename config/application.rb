5c41913c1a4a61be59e5bd1474af4c698f27c4bd
d1eg0dev <diego@studiowd.com.mx>
2022-08-19 20:07:19 -0500

    autoload

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module Serenity
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    onfig.autoloader = :zeitwerk
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
