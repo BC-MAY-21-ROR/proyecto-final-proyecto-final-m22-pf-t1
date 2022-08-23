5563730548769a4ad07a224ce09cc5e9d0b7edf2
d1eg0dev <diego@studiowd.com.mx>
2022-08-23 17:03:43 -0500

    Seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,5 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Category.create(name: "Productos")
+Category.create(name: "Servicios")
